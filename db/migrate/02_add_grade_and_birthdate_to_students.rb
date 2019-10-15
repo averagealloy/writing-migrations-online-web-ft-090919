63d98f8965908fece8d3bb55094f27d0d512c44a
Mike McTighe <m.mctighe10@gmail.com>
2019-10-15 21:52:23 +0000

    Automatically backed up by Learn

--- db/migrate/02_add_grade_and_birthdate_to_students.rb
+++ db/migrate/02_add_grade_and_birthdate_to_students.rb
@@ -1,4 +1,4 @@
-class AddFavori < ActiveRecord::Migration[5.2]
+class AddFav < ActiveRecord::Migration[5.2]
  def change
    add_column :students, :grade, :integer
    add_column :students, :birthdate, :string
